573f13582b0a3da460be2a456b961ac9f5983140
Irina Popova <irisha9753@gmail.com>
2020-05-26 10:49:28 -0700

    Add project files.

---
+++ SeleniumWebDrowerCourse/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumWebDrowerCourse")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumWebDrowerCourse")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2e696d8-e5d8-478e-9424-ba349dea04b0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumWebDrowerCourse/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+	[TestFixture]
+	public class MyFirstTest
+	{
+		private IWebDriver driver;
+		private WebDriverWait wait;
+
+		[SetUp]
+		public void start()
+		{
+			
+			
+			var o = new ChromeOptions();
+			o.AddArgument("-disable-features=RendererCodeIntegrity");			
+			driver = new ChromeDriver(o);
+			driver.Manage().Window.Maximize();
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		}
+
+		[Test]
+		public void FirstTest()
+		{
+			driver.Url = "http://www.google.com/";
+			driver.FindElement(By.Name("q")).SendKeys("webdriver");
+			driver.FindElement(By.Name("btnK")).Click();
+			wait.Until(ExpectedConditions.TitleIs("webdriver - Поиск в Google"));
+		}
+
+		[TearDown]
+		public void stop()
+		{
+			driver.Quit();
+			driver = null;
+		}
+	}
+}
