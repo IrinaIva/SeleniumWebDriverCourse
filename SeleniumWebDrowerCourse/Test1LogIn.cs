c4a8d9eda61bea0a55722b41ba43e8b46e4d9757
Irina Popova <irisha9753@gmail.com>
2020-06-27 13:48:37 -0700

    Hw7
    HW7

--- SeleniumWebDrowerCourse/Test1LogIn.cs
+++ SeleniumWebDrowerCourse/Test1LogIn.cs
@@ -4,6 +4,17 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System.Threading;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support;
+using System.Windows.Input;
+using System.IO;
+using System.Reflection;
+
 
 namespace csharp_example
 {
@@ -26,27 +37,63 @@ namespace csharp_example
 		}
 
 		[Test]
-		public void FirstTest()
+		public void LogInTest()
+		{
+			OpenMainPage();
+			Login("admin", "admin");
+			wait.Until(ExpectedConditions.TitleIs("My Store"));
+			AssertLogin();
+			Logout();
+		}
+		[Test]
+		public void ClickToAllSections()
+		{
+			OpenMainPage();
+			Login("admin", "admin");
+			wait.Until(ExpectedConditions.TitleIs("My Store"));
+			AssertLogin();			
+			ClickAllMenus();	
+
+		}
+		public void ClickAllMenus()
+		{
+			int count = driver.FindElements(By.CssSelector("#app- .name")).Count;
+			for (int i = 1; i < count + 1; i++)
+			{				
+				driver.FindElement(By.XPath("(//li[@id='app-']/a/span[2])[" + i + "]")).Click();
+				IsElementPresent(By.CssSelector(".fa - stack icon - wrapper"));
+				int count2 = driver.FindElements(By.CssSelector("[id^='doc-']")).Count;
+				if (count2 > 0)
+				{
+					for (int j = 1; j < count2 + 1; j++)
+					{
+						driver.FindElement(By.XPath("//li[starts-with(@id,'doc-')]["+j+"]")).Click();
+						IsElementPresent(By.CssSelector(".fa - stack icon - wrapper"));
+					}
+				}			
+			}
+		}
+
+		public void OpenMainPage()
 		{
 			driver.Url = "http://localhost/litecart/admin";
 			wait.Until(ExpectedConditions.TitleIs("My Store"));
-			Login("admin", "admin");
-			AssertLogin();
-			Logout();
 		}
 		public void Login(string id, string password)
 		{
 			driver.FindElement(By.Name("username")).SendKeys(id);
 			driver.FindElement(By.Name("password")).SendKeys(password);
 			driver.FindElement(By.Name("login")).Click();
+			if (IsElementPresent(By.CssSelector(".fa-sign-out")) == false)
+				driver.FindElement(By.Name("login")).Click();
 			Thread.Sleep(500);
 		}
 		public void AssertLogin()
-		{			
-			Assert.IsTrue(IsElementPresent(By.CssSelector(".fa-sign-out")));
-		}		
+		{
+			Assert.IsFalse(IsElementPresent(By.CssSelector(".fa-sign-out[")));
+		}
 		public void Logout()
-		{			
+		{
 			driver.FindElement(By.CssSelector(".fa-sign-out")).Click();
 		}
 		public bool IsElementPresent(By by)
@@ -56,10 +103,15 @@ namespace csharp_example
 				driver.FindElement(by);
 				return true;
 			}
-			catch (NoSuchElementException e)
+			//catch (InvalidSelectorException ex)
+			//{
+			//	throw ex;
+			//}
+			catch (NoSuchElementException ex)
 			{
 				return false;
 			}
+			//	return driver.FindElements(by).Count > 0;
 		}
 		[TearDown]
 		public void stop()
